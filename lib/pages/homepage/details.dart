f98cd769be4d36e8bdc086ca7dbaff597c0d39ad
Adnan-AbdulRahman <adnanabdulrahman0544@gmail.com>
2026-06-11 14:34:50 +0000

    4th

--- lib/pages/homepage/details.dart
+++ lib/pages/homepage/details.dart
@@ -24,7 +24,7 @@ class ProjectDetails extends StatelessWidget {
     if (await canLaunchUrl(uri)) {
       await launchUrl(uri, mode: LaunchMode.externalApplication);
     } else {
-      throw 'Could not launch $url';
+      throw 'Could not launcch $url';
     }
   }
 
